0a428072cf992eaaa1bc7422cf07dafee1a48589
monofrio <onofri04@Gmail.com>
2014-11-03 10:44:03 -0500

    app.js

--- controllers/app.js
+++ controllers/app.js
@@ -1,17 +1,68 @@
 /**
- * Created by Gattermeier on 10/23/14.
+ * Created by M. Gattermeier on 10/23/14.
+ * http://www.gattermeier.net
  */
+
 requirejs.config({
     paths: {
-        jquery: ['//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js','lib/jquery-2.1.1.min'],
-        bootstrap: ['//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min','./lib/bootstrap.min']
+         jquery: ['//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min','../libs/jquery/dist/jquery.min']
+        ,bootstrap: ['//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min','../libs/bootstrap/dist/js/bootstrap.min']
+        ,offcanvas:'../libs/jasny-bootstrap/js/offcanvas'
+        ,'jquery-cookie': '../libs/jquery-cookie/jquery-cookie'
+        ,jq_onload: 'jq_onload'
+        ,jq_social: 'jq_social'
+        ,jq_events: 'jq_events' // ADL custom jquery based DOM interactions
+        ,events: 'events' // events without dependencies
     },
     shim: {
-        'bootstrap':{deps: ['jquery']}
+         'bootstrap':{deps: ['jquery']}
+        ,'offcanvas': {deps: ['jquery']}
+        ,'jquery-cookie':{deps: ['jquery']}
+        ,'jq_onload': {deps: ['jquery']}
+        ,'jq_social': {deps: ['jquery']}
+        ,'jq_events':{deps: ['jquery']}
     }
 });
- 
-requirejs(['jquery','bootstrap'], function($){
-    // here goes all JS code depending on jquery and bootstrap 
+
+requirejs(['events'], function( _e ) {
+    // non depended events code
     return {};
-}); 
+});
+
+requirejs(['jquery','bootstrap','offcanvas'], function($,_bootstrap,_off) {
+
+    console.log('off canvas loaded');
+    return {}
+
+});
+
+requirejs(['jquery','bootstrap', 'jq_events'], function( $ , _bootstrap , _jqe ) {
+    // here goes all JS code depending on jquery and bootstrap
+    // events is the place standard jquery dependend DOM interactions live
+
+    $("#socialmediabar2014 .nav-tabs a").click(function (e) {
+        var tab = $(this);
+        if (tab.parent('li').hasClass('active')) {
+            window.setTimeout(function () {
+                $(".tab-pane").removeClass('active');
+                tab.parent('li').removeClass('active');
+            }, 1);
+        }
+    });
+
+    // social media widget for left sidebar
+
+
+
+    // load G+ in the social media widget explicitly
+    $("#socialmediabar2014 a.googleplus").click(function (e) {
+        gapi.plus.go();
+    });
+
+
+
+    console.log('finished');
+    return {};
+});
+
+
